b24a6f0aacc1dbb44d36d92e27ed760140f25905
Boushra-Ja <boshrah0912@gmail.com>
2023-05-02 15:04:42 +0300

    update 4

--- app/Http/Resources/Boshra/ChildResourse.php
+++ app/Http/Resources/Boshra/ChildResourse.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Boshra;
 
 use App\Models\EductionalCondition;
 use App\Models\MedicalCondition;
+use App\Models\MemberFamily;
 use App\Models\PersonalInformation;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -14,6 +15,7 @@ class ChildResourse extends JsonResource
     public function toArray(Request $request): array
     {
         $personal_answers = PersonalInformation::where('child_id'  ,$this->id)->get() ;
+        $family = MemberFamily::where('child_id' , $this->id)->get() ;
         $medical_answers = MedicalCondition::where('child_id'  ,$this->id)->get() ;
         $eductional_answers = EductionalCondition::where('child_id'  ,$this->id)->get() ;
 
@@ -22,6 +24,7 @@ class ChildResourse extends JsonResource
             'name' => $this->name ,
             'age' =>$this->age ,
             'phone' => $this->phone_num ,
+            'family' => FamilyResource::collection($family) ,
             'personal_info' => PersonalInfoResourse::collection($personal_answers),
             'medical_info' => MedicalInfoResourse::collection($medical_answers) ,
             'eductional_info' => EducationInfoResourse::collection($eductional_answers),
