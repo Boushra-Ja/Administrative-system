59e9c6f9374a25241f9faf5bbedc9763b31bda5d
Boushra-Ja <boshrah0912@gmail.com>
2023-07-29 06:24:47 +0300

    notifiaction 1

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -6,6 +6,7 @@ use App\Events\NotificationEvent;
 use App\Http\Controllers\API\BaseController;
 use App\Models\ChildNotification;
 use App\Models\Notification;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class NotificationController extends BaseController
@@ -47,6 +48,11 @@ class NotificationController extends BaseController
     public function all_employee_notifications($emp_id) {
 
         $notifications = Notification::where('receiver_id' , $emp_id)->get() ;
+        return $this->sendResponse($notifications , 'success in get all notifications') ;
+    }
+    public function all_admin_notifications($admin_id) {
+
+        $notifications = Notification::where('receiver_id' , $admin_id)->get() ;
         return $this->sendResponse($notifications , 'success in get all notifications') ;
     }
 }
