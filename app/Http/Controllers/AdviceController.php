52bda2ad85a090ea0a3002eb1f051100b3d2fddf
BatoulAlZoubi <faizzoubi10@gmail.com>
2023-07-29 05:52:07 +0300

    reportttt

--- app/Http/Controllers/AdviceController.php
+++ app/Http/Controllers/AdviceController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\NotificationEvent;
 use App\Http\Controllers\API\BaseController;
 use App\Models\Advice;
 use App\Http\Requests\StoreAdviceRequest;
 use App\Http\Requests\UpdateAdviceRequest;
 use App\Http\Resources\Boshra\AdviceResource;
+use App\Models\Notification;
 
 class AdviceController extends BaseController
 {
@@ -37,9 +39,18 @@ class AdviceController extends BaseController
 
             'child_id' => $request->child_id ,
             'text' => $request->text
-        ]) ;
+        ]);
+        $advice_id=Advice::where('child_id',$request->child_id)->value("id");
         if($advice)
         {
+            broadcast(new NotificationEvent("ارسال نصيحه",  "تم ارسال بعض النصائح لطفلكم",$request->child_id,$advice_id));
+            $realTime = Notification::create([
+                'title' => "ارسال نصيحه",
+                'receiver_id' =>$request->child_id,
+                'message' => "تم ارسال بعض النصائح لطفلكم",
+
+            ]);
+            $realTime->save();
             return $this->sendResponse($advice , "success in add advice..");
         }
         return $this->sendErrors([] , 'error in added advice' ) ;
